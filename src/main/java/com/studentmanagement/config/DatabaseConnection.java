c2c2d86ebad928a151ee89a1eb83cc8f5a61dbb3
Himanshu Nishad <himanshunshd5678@gmail.com>
2026-08-18 20:45:52 +0530

    Remove database password from source code

--- src/main/java/com/studentmanagement/config/DatabaseConnection.java
+++ src/main/java/com/studentmanagement/config/DatabaseConnection.java
@@ -5,9 +5,9 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
 
-   final private static String URL = "jdbc:mysql://localhost:3306/student_management";
+   final private static String URL = "jdbc:mysql://localhost:3306/student_management";      // When a variable is final and initialized with a fixed value, Java naming conventions recommend UPPER_CASE names for constants.
    final private static String ROOT = "root";
-   final private static String PASSWORD = "MAnsi@123";          //When a variable is final and initialized with a fixed value, Java naming conventions recommend UPPER_CASE names for constants.
+   final private static String PASSWORD = "your_password";       // Add your MySQL password .
 
     public static Connection getConnection(){                  //static is useful because we don't need to create a new DatabaseConnection object every time.
            try{     
